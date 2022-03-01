ad99e77c70e12f3742650738f776324a1ba1a59c
matthew-nagy <matttnagy@gmail.com>
2022-03-01 13:19:20 +0000

    ?

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -489,7 +489,7 @@ float collision(t_param*const restrict params, const CellList cells, CellList tm
   __assume((params->ny % 64) == 0);
   __assume((params->ny % 128) == 0);
   __assume(params->ny >= 128);
-  #pragma omp parallel for reduction(+:tot_cells), reduction(+:tot_u)
+  #pragma omp parallel for reduction(+:tot_u)
   for (int jj = 0; jj < params->ny; jj+=1)
   {
     int y_n = (jj + 1) & params->nyBitMask;
