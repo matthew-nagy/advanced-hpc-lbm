ebc2077f84123faed4e925306d72e2215ce15bfc
matthew-nagy <matttnagy@gmail.com>
2022-03-01 17:26:56 +0000

    SIMD the accelerate assignment

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -249,6 +249,7 @@ int accelerate_flow(const t_param params, CellList cells, int const*const restri
   for (int ii = 0; ii < numOfSecondRowNonObs; ii++)
   {
     const int index = secondRowNonObs[ii];
+    #pragma omp simd aligned(cells : 64)
     for(int i = 1; i < 9; i++){
       cells[i][index] += changes[i];
     }
