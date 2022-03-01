f2b7fae649a759478d71fedbd5960317639d663e
matthew-nagy <matttnagy@gmail.com>
2022-03-01 17:24:36 +0000

    Test for accelerate flow

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -244,9 +244,8 @@ int accelerate_flow(const t_param params, CellList cells, int const*const restri
   float w1 = params.density * params.accel * (1.0/9.f);
   float w2 = params.density * params.accel * (1.0f/36.f);
 
-  /*
+  
   const float changes[9] = {0.0f, w1, 0.0f, w1, 0.0f, w2, w2, w2, w2};
-
   for (int ii = 0; ii < numOfSecondRowNonObs; ii++)
   {
     const int index = secondRowNonObs[ii];
@@ -254,22 +253,22 @@ int accelerate_flow(const t_param params, CellList cells, int const*const restri
       cells[i][index] += changes[i];
     }
   }
-  */
+  
 
-  for (int ii = 0; ii < numOfSecondRowNonObs; ii++)
-  {
-    /* if the cell is not occupied and
-    ** we don't send a negative density */
-      /* increase 'east-side' densities */
-      int index = secondRowNonObs[ii];
-      cells[1][index] += w1;
-      cells[5][index] += w2;
-      cells[8][index] += w2;
-      /* decrease 'west-side' densities */
-      cells[3][index] -= w1;
-      cells[6][index] -= w2;
-      cells[7][index] -= w2;
-  }
+  // for (int ii = 0; ii < numOfSecondRowNonObs; ii++)
+  // {
+  //   /* if the cell is not occupied and
+  //   ** we don't send a negative density */
+  //     /* increase 'east-side' densities */
+  //     int index = secondRowNonObs[ii];
+  //     cells[1][index] += w1;
+  //     cells[5][index] += w2;
+  //     cells[8][index] += w2;
+  //     /* decrease 'west-side' densities */
+  //     cells[3][index] -= w1;
+  //     cells[6][index] -= w2;
+  //     cells[7][index] -= w2;
+  // }
 
   return EXIT_SUCCESS;
 }
