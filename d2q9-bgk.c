6f503e0a2f033be8bd31cdc71f01e32a0ec40161
matthew-nagy <matttnagy@gmail.com>
2022-04-12 18:45:18 +0100

    Better velocity maybe

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -782,7 +782,7 @@ int initialise(const char* paramfile, const char* obstaclefile,
     }
     params.totCells -= 1.f;
 
-    int adjustedY = yy - myRank.rowStartOn;
+    int adjustedY = yy - (myRank.rowStartOn + 1);
     if(adjustedY >=0 && adjustedY < params.ny)
       (*obstacles_ptr)[xx + adjustedY * params.nx] = blocked;
 
