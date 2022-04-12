5176ea4a0c214e9b8a4ec15ddab8df6ef4aeab66
matthew-nagy <matttnagy@gmail.com>
2022-04-12 16:47:05 +0100

    Making the accelerate worse on purpose to see if its better

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -365,7 +365,6 @@ float timestep(int const*const restrict obstacles)
   return collision(obstacles);
 }
 
-int printed = 0;
 
 int accelerate_flow(int const*const restrict obstacles)
 {
@@ -373,18 +372,11 @@ int accelerate_flow(int const*const restrict obstacles)
   if(rank != (nprocs - 1))
     return EXIT_SUCCESS;
 
-  if(!printed){
-    printed = 1;
-
-    printf("accel %d\n",  myRank.rowStartOn + params.ny - 4);
-
-  }
-
   /* compute weighting factors */
   float w1 = params.density * params.accel * (1.0/9.f);
   float w2 = params.density * params.accel * (1.0f/36.f);
   
-  int jj = params.ny - 3;
+  int jj = params.ny - 2;
 
 
   for (int ii = 1; ii < params.nx - 1; ii++)
