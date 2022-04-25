f519fb797753a280de47b375fa4180f03987e833
matthew-nagy <matttnagy@gmail.com>
2022-04-26 00:02:23 +0100

    This is why I should use c++

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -956,7 +956,7 @@ int write_values(char* obstacles, float* av_vels)
       }
 
       /* write to file */
-      fprintf(fp, "%d %d %.12E %.12E %.12E %.12E %d\n", ii, jj, u_x, u_y, u, pressure, int(fullObstacles[ii * fullGridWidth + jj]));
+      fprintf(fp, "%d %d %.12E %.12E %.12E %.12E %d\n", ii, jj, u_x, u_y, u, pressure, (int)fullObstacles[ii * fullGridWidth + jj]);
     }
   }
 
