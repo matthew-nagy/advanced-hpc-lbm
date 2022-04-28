a82df8dadadc367dd8bdac3d7f8c3d661bfe574a
matthew-nagy <matttnagy@gmail.com>
2022-04-28 17:18:49 +0100

    suck on my cock and balls

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -363,7 +363,7 @@ int main(int argc, char* argv[])
   
   /* write final values and free memory */
   printf("==done==\n");
-  printf("Reynolds number:\t\t%.12E\n", calc_reynolds(obstacles));
+  //printf("Reynolds number:\t\t%.12E\n", calc_reynolds(obstacles));
   printf("Elapsed Init time:\t\t\t%.6lf (s)\n",    init_toc - init_tic);
   printf("Elapsed Compute time:\t\t\t%.6lf (s)\n", comp_toc - comp_tic);
   //printf("%.6lf\n", comp_toc - comp_tic);
